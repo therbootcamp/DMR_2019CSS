6e7ddfec03510bf88f6d63dc91c4dc46b7faf9bb
mdsteiner <markus.steiner@unibas.ch>
2019-12-07 14:32:56 +0100

    updated wrangling and kc_house data

--- _sessions/WranglingII/WranglingII_Interactive.R
+++ _sessions/WranglingII/WranglingII_Interactive.R
@@ -7,26 +7,27 @@ kc_house <- read_csv(file = "1_Data/kc_house.csv")
 
 # Datensatz vorbereiten (Aufgaben B1, B4-B6)
 kc_house <- kc_house %>%
-  mutate(living_sqm = sqft_living * 0.093,
-         above_sqm = sqft_above * 0.093,
-         basement_sqm  = sqft_basement * 0.093,
-         total_sqm = living_sqm + above_sqm + basement_sqm,
-         mansion = case_when(
-           total_sqm > 750 ~ "yes",
-           total_sqm <= 750~ "no"))
+  mutate(qm_wohnraum = qf_wohnraum * 0.093,
+         qm_dachstock = qf_dachstock * 0.093,
+         qm_keller  = qf_keller * 0.093,
+         qm_total = qm_wohnraum + qm_dachstock + qm_keller,
+         villa = case_when(
+           qm_total > 750 ~ "ja",
+           qm_total <= 750~ "nein"))
 
-# C7. Berechne die mittlere Anzahl Stockwerke (`floors`) und Badezimmer
-# (`bathrooms`) von Villen (`mansion`).
+# C7. Berechne die mittlere Anzahl Stockwerke (`stoecke`) und Badezimmer
+# (`badezimmer`) von Villen (`villa`).
 
 
-# D5. Kreiere eine neue Variable `built_decade`, welche angibt, in welchem
-# Jahrzehnt ein Haus gebaut wurde und erstelle dann die Tabelle der letzten
-# Aufgabe Gruppiert nach dieser neuen Variable. Tipp: die `floor()` Funktion
-# rundet eine Zahl auf die nächst kleinere ganze Zahl ab.
+# D5. Diese Tabelle hat recht viele Zeilen und ist daher etwas unübersichtlich.
+# Kreiere eine neue Variable `baujahrzehnt`, welche angibt, in welchem Jahrzehnt
+# ein Haus gebaut wurde und erstelle dann die Tabelle der letzten Aufgabe Gruppiert
+# nach dieser neuen Variable. Tipp: die `floor()` Funktion rundet eine Zahl auf
+# die nächst kleinere ganze Zahl ab.
 
 
 # E1. Dein Freund Theodorus interessiert sich für Häuser einer bestimmten Gegend,
-# nämlich mit den Postleitzahlen (`zipcode`) 98001, 98109, 98117 und 98199.
+# nämlich mit den Postleitzahlen (`postleitzahl`) 98001, 98109, 98117 und 98199.
 # Stelle ihm einen neuen Datensatz `theodorus` zusammen, welcher nur die Häuser
 # dieser Gegend enthält. Tipp: der `%in%` Operator testet ob Elemente des Vektors
 # auf der rechten Seite im Vektor auf der linken Seite enthalten sind.
@@ -37,4 +38,4 @@ kc_house <- kc_house %>%
 # Postleitzahlen und ob es eine Villa ist.
 
 # 1. Welche Postleitzahl hat den höchsten Anteil Häuser, welche am Wasser gebaut
-# sind (`waterfront`)? Printe nur diese Zeile des Datensatzes.
+# sind (`ufer`)? Printe nur diese Zeile des Datensatzes.
