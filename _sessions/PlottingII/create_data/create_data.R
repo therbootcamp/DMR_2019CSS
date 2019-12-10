8b7008bccab2f1c31b5a6943221df88d130a3f64
Dirk Wulff <dirkwulff@fp-ws-b210.psycho.unibas.ch>
2019-12-10 23:20:08 +0100

    ..

--- _sessions/PlottingII/create_data/create_data.R
+++ _sessions/PlottingII/create_data/create_data.R
@@ -14,6 +14,16 @@ verbrechen = crime %>%
          prozent_pension = pctWRetire,
          prozent_armut = PctPopUnderPov,
          prozent_arbeitslos = PctUnemployed,
-         prozent_metro = PctUsePubTrans)
+         prozent_metro = PctUsePubTrans) %>%
+  mutate(verbrechen = case_when(
+    verbrechen == 'arsons' ~ 'brandstiftung',
+    verbrechen == 'assaults' ~ 'körperverletzung',
+    verbrechen == 'autoTheft' ~ 'autodiebstahl',
+    verbrechen == 'burglaries' ~ 'einbruch',
+    verbrechen == 'larcenies' ~ 'diebstahl',
+    verbrechen == 'murders' ~ 'mord',
+    verbrechen == 'assaults' ~ 'körperverletzung',
+    verbrechen == 'robberies' ~ 'raubüberfall',
+  ))
 
 write_csv(verbrechen,'_sessions/PlottingII/1_Data/verbrechen.csv')
